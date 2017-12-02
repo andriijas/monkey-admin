dd09cfa372383432f8082a3ba1a9fa96ec0b892d
Andreas Cederström <andriijas@users.noreply.github.com>
2017-12-02 17:01:03 +0100

    commonchunks / long term caching test

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,43 @@
 const path = require("path");
-const webpack = require("webpack");
 
 const { injectBabelPlugin } = require("react-app-rewired");
 //const rewireLess = require("react-app-rewire-less");
 const rewireLess = require("react-app-rewire-less-modules");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+// Temporary untill https://github.com/facebookincubator/create-react-app/pull/3145 is merged
+const webpack = require("webpack");
+const paths = require("react-scripts/config/paths");
+const NameAllModulesPlugin = require("name-all-modules-plugin");
+const vendors = [
+  "classnames",
+  "dva",
+  "dva-loading",
+  "moment",
+  "prop-types",
+  "react",
+  "react-dom",
+  "react-router-dom",
+  "react-router-redux",
+  "react-redux",
+  "redux",
+  "redux-saga"
+];
+
+/**
+ * Retrieves plugin's index in webpack configuration or returns null
+ * @param {Object} config - webpack configuration
+ * @param {string} name - Plugin name
+ * @returns {number|null}
+ */
+function getPluginIndex(config, name) {
+  const index = config.plugins.findIndex(
+    plugin => plugin.constructor.name === name
+  );
+  return index !== -1 ? index : null;
+}
 
 module.exports = function override(config, env) {
   if (env === "development") {
@@ -15,12 +49,61 @@ module.exports = function override(config, env) {
     config
   );
 
+  // Temporary untill https://github.com/facebookincubator/create-react-app/pull/3145 is merged
+  if (env === "production") {
+    config.entry = {
+      main: paths.appIndexJs,
+      polyfills: require.resolve("react-scripts/config/polyfills"),
+      vendors
+    };
+
+    config.plugins.push(new webpack.NamedModulesPlugin());
+
+    config.plugins.push(
+      new webpack.NamedChunksPlugin(chunk => {
+        if (chunk.name) {
+          return chunk.name;
+        }
+        const chunkNames = chunk.mapModules(m => m);
+        chunkNames.sort((chunkA, chunkB) => chunkA.depth - chunkB.depth);
+        const fileName = chunkNames[0].resource;
+        return path.basename(fileName, path.extname(fileName));
+      })
+    );
+
+    config.plugins.push(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendors",
+        minChunks: Infinity
+      })
+    );
+
+    config.plugins.push(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "runtime"
+      })
+    );
+
+    config.plugins.push(new NameAllModulesPlugin());
+
+    const etpIndex = getPluginIndex(config, "ExtractTextPlugin");
+    if (etpIndex) {
+      config.plugins[etpIndex].options.allChunks = true;
+    }
+
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static"
+      })
+    );
+  }
+
   return rewireLess.withLoaderOptions(
     `${env === "production" ? "app" : "[local]"}-[hash:base64:8]`,
     {
       modifyVars: {
         "@primary-color": "#61dafb",
-        "@info-color": "#61dafb",
+        "@info-color": "#61dafb"
       }
     }
   )(config, env);
