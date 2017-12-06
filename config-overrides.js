9a342d4b27bb826826f1030df5cd9ac3485de9f6
Andreas Cederström <andriijas@users.noreply.github.com>
2017-12-06 14:01:24 +0100

    upgrades

--- config-overrides.js
+++ config-overrides.js
@@ -1,43 +1,12 @@
 const path = require("path");
 
-const { injectBabelPlugin } = require("react-app-rewired");
+const { injectBabelPlugin, compose } = require("react-app-rewired");
 //const rewireLess = require("react-app-rewire-less");
 const rewireLess = require("react-app-rewire-less-modules");
+const rewireVendorSplitting = require("react-app-rewire-vendor-splitting");
 
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
-
-// Temporary untill https://github.com/facebookincubator/create-react-app/pull/3145 is merged
-const webpack = require("webpack");
-const paths = require("react-scripts/config/paths");
-const NameAllModulesPlugin = require("name-all-modules-plugin");
-const vendors = [
-  "classnames",
-  "dva",
-  "dva-loading",
-  "moment",
-  "prop-types",
-  "react",
-  "react-dom",
-  "react-router-dom",
-  "react-router-redux",
-  "react-redux",
-  "redux",
-  "redux-saga"
-];
-
-/**
- * Retrieves plugin's index in webpack configuration or returns null
- * @param {Object} config - webpack configuration
- * @param {string} name - Plugin name
- * @returns {number|null}
- */
-function getPluginIndex(config, name) {
-  const index = config.plugins.findIndex(
-    plugin => plugin.constructor.name === name
-  );
-  return index !== -1 ? index : null;
-}
 
 module.exports = function override(config, env) {
   if (env === "development") {
@@ -49,62 +18,27 @@ module.exports = function override(config, env) {
     config
   );
 
-  // Temporary untill https://github.com/facebookincubator/create-react-app/pull/3145 is merged
   if (env === "production") {
-    config.entry = {
-      main: paths.appIndexJs,
-      polyfills: require.resolve("react-scripts/config/polyfills"),
-      vendors
-    };
-
-    config.plugins.push(new webpack.NamedModulesPlugin());
-
-    config.plugins.push(
-      new webpack.NamedChunksPlugin(chunk => {
-        if (chunk.name) {
-          return chunk.name;
-        }
-        const chunkNames = chunk.mapModules(m => m);
-        chunkNames.sort((chunkA, chunkB) => chunkA.depth - chunkB.depth);
-        const fileName = chunkNames[0].resource;
-        return path.basename(fileName, path.extname(fileName));
-      })
-    );
-
-    config.plugins.push(
-      new webpack.optimize.CommonsChunkPlugin({
-        name: "vendors",
-        minChunks: Infinity
-      })
-    );
-
-    config.plugins.push(
-      new webpack.optimize.CommonsChunkPlugin({
-        name: "runtime"
-      })
-    );
-
-    config.plugins.push(new NameAllModulesPlugin());
-
-    const etpIndex = getPluginIndex(config, "ExtractTextPlugin");
-    if (etpIndex) {
-      config.plugins[etpIndex].options.allChunks = true;
-    }
-
     config.plugins.push(
       new BundleAnalyzerPlugin({
-        analyzerMode: "static"
+        analyzerMode: "static",
+        openAnalyzer: false
       })
     );
   }
 
-  return rewireLess.withLoaderOptions(
-    `${env === "production" ? "app" : "[local]"}-[hash:base64:8]`,
-    {
-      modifyVars: {
-        "@primary-color": "#61dafb",
-        "@info-color": "#61dafb"
+  const rewires = compose(
+    rewireLess.withLoaderOptions(
+      `${env === "production" ? "app" : "[local]"}-[hash:base64:8]`,
+      {
+        modifyVars: {
+          "@primary-color": "#61dafb",
+          "@info-color": "#61dafb"
+        }
       }
-    }
-  )(config, env);
+    ),
+    rewireVendorSplitting
+  );
+
+  return rewires(config, env);
 };
---
+++ src/vendors.js
@@ -0,0 +1,16 @@
+module.exports = {
+  vendors: [
+    "classnames",
+    "dva",
+    "dva-loading",
+    "moment",
+    "prop-types",
+    "react",
+    "react-dom",
+    "react-router-dom",
+    "react-router-redux",
+    "react-redux",
+    "redux",
+    "redux-saga"
+  ]
+};
